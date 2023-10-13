f5273d9b05c34c382e3a5f092db6fbb223286d0b
Ariel Campos <ariel.campos94@yahoo.com>
2023-10-13 17:45:13 -0300

    empty page finished

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import { FiPlusCircle } from 'react-icons/fi'
+import logo from '../assets/logo.png'
+import empty from '../assets/empty.svg'
+import styles from './home.module.css'
+
+export function Home(){
+
+
+return (
+    <div>
+        <header>
+            <img src={logo} alt="logo to-do list" />
+        </header>
+
+        <div className={styles.container}>
+            <main>
+                <form>
+                    <input type="text" placeholder='Adicione uma nova tarefa'/>
+
+                    <button type="submit">   
+                        Criar
+                        <FiPlusCircle/>
+                    </button>
+                </form>
+
+                <div className={styles.table}>
+                    <div className={styles.tableHeader}>
+                        <div className={styles.tasksCreated}>
+                            <p>Tarefas criadas</p>
+                            <p>0</p>
+                        </div>
+
+                        <div className={styles.tasksFinished}>
+                            <p>Concluídas</p>
+                            <p>0</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.tableBody}>
+                        <img src={empty}/>
+                        <div>
+                            <p><strong>Você ainda não tem tarefas cadastradas</strong></p>
+                            <p>Crie tarefas e organize seus itens a fazer</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+        </div>
+    </div>
+    );
+}
