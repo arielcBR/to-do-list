957dea3ddf3b88b569f6127a905285b1b372c342
Ariel Campos <ariel.campos94@yahoo.com>
2023-10-14 02:18:17 -0300

    created tasks counter is done

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,17 +1,26 @@
 import { RiDeleteBin6Line } from 'react-icons/ri'
 import styles from './task.module.css'
+import { FormEvent } from 'react';
 
 interface TaskProps {
     content: string;
 }
 
 export function Task({content}: TaskProps){
+  
+    function handleDeleteTask(event: FormEvent){
+        event.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleDeleteTask}>
             <label className={styles.form} htmlFor="taskSelected">
                 <input className={styles.input} type="radio" id="taskSelected" value=""/>
                 <p className={styles.description}>{content}</p>
-                <button className={styles.trashButton} type="submit">
+                <button 
+                    className={styles.trashButton} 
+                    type="submit"
+                >
                     <RiDeleteBin6Line/>
                 </button>
             </label>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,41 @@
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 import { FiPlusCircle } from 'react-icons/fi'
+import { v4 as uuidv4 } from 'uuid';
 import logo from '../assets/logo.png'
 import empty from '../assets/empty.svg'
 import styles from './home.module.css'
 
 import { Task } from '../components/Task/Task'
 
+
 export function Home(){
+    const [tasks, setTasks] = useState<Array<string>>([]);
+    const [newTask, setNewTask] = useState("");
+    const [createdTasksCounter, setCreatedTasksCounter] = useState(0);
+    // const [finishedTasksCounter, setFinishedTasksCounter] = useState(0);
+
+    const isNewTaskEmpty = !newTask.length;
+
+    function handleNewTask(event: ChangeEvent<HTMLInputElement>){
+        event.target.setCustomValidity("");
+        setNewTask(event.target.value);
+    }
+    
+    function handleCreateNewTask(event: FormEvent){
+        event.preventDefault();
+        const taskValidation = newTask.trim();
+        
+        if(taskValidation.length) {
+            setTasks([...tasks, newTask]);
+            setCreatedTasksCounter(tasks.length + 1);
+            setNewTask("");
+        }
+    }
+
+    function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>){
+        event.target.setCustomValidity("Este campo é obrigatório!");
+    }
+
 
 
 return (
@@ -16,10 +46,17 @@ return (
 
         <div className={styles.container}>
             <main>
-                <form>
-                    <input type="text" placeholder='Adicione uma nova tarefa'/>
+                <form onSubmit={handleCreateNewTask}>
+                    <input 
+                        type="text" 
+                        placeholder='Adicione uma nova tarefa'
+                        value={newTask}
+                        onChange={handleNewTask}
+                        onInvalid={handleNewTaskInvalid}
+                        required
+                    />
 
-                    <button type="submit">   
+                    <button type="submit" disabled={isNewTaskEmpty}>   
                         Criar
                         <FiPlusCircle/>
                     </button>
@@ -29,7 +66,7 @@ return (
                     <div className={styles.tableHeader}>
                         <div className={styles.tasksCreated}>
                             <p>Tarefas criadas</p>
-                            <p>0</p>
+                            <p>{createdTasksCounter}</p>
                         </div>
 
                         <div className={styles.tasksFinished}>
@@ -39,14 +76,24 @@ return (
                     </div>
 
                     <div className={styles.tableBody}>
-                        <Task content={'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.'}/>
-                        {/* <div className={styles.wrapperEmptyState}>
-                            <img src={empty}/>
-                            <div>
-                                <p><strong>Você ainda não tem tarefas cadastradas</strong></p>
-                                <p>Crie tarefas e organize seus itens a fazer</p>
-                            </div>
-                        </div> */}
+                    {!tasks.length
+                        ? 
+                        (
+                            <div className={styles.wrapperEmptyState}>
+                                <img src={empty}/>
+                                <div>
+                                    <p><strong>Você ainda não tem tarefas cadastradas</strong></p>
+                                    <p>Crie tarefas e organize seus itens a fazer</p>
+                                </div>
+                            </div>)  
+                        : 
+                        (
+                            tasks.map(task => (
+                                <Task key={uuidv4()} content={task}/>
+                            ))
+                        )
+                    }
+                        
                     </div>
                 </div>
             </main>
